5e3891cbec5bb88cd01a27751e23478acd08dc24
Keith Davidson <kbdsp@hotmail.com>
2021-10-23 22:36:01 -0400

    Start of 95, made game over screen use Dimensions

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { View, Button, Image, StyleSheet, Text } from 'react-native';
+import {
+    View,
+    Button,
+    Image,
+    StyleSheet,
+    Text,
+    Dimensions,
+    ScrollView
+} from 'react-native';
 
 import Card from '../components/Card';
 import BodyText from '../components/BodyText';
@@ -10,25 +18,27 @@ import Colors from '../constants/colors';
 
 const GameOverScreen = props => {
     return (
-        <View style={styles.screen}>
-            <TitleText>The Game Is Over</TitleText>
-            <View style={styles.imageContainer}>
-                <Image
-                    source={require('../assets/success.png')}
-                    // Network image test - note this does not display in the android x86 emulator
-                    // source={{uri:'https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg'}}
-                    style={styles.image}
-                    resizeMode="cover"  // Note cover is the default
-                />
+        <ScrollView>
+            <View style={styles.screen}>
+                <TitleText>The Game Is Over!</TitleText>
+                <View style={styles.imageContainer}>
+                    <Image
+                        source={require('../assets/success.png')}
+                        // Network image test - note this does not display in the android x86 emulator
+                        // source={{uri:'https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg'}}
+                        style={styles.image}
+                        resizeMode="cover"  // Note cover is the default
+                    />
+                </View>
+                <View style={styles.resultContainer}>
+                    <BodyText style={styles.resultText}>
+                        Your phone needed <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to guess the
+                        number <Text style={styles.highlight}>{props.userNumber}</Text>.
+                    </BodyText>
+                </View>
+                <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
             </View>
-            <View style={styles.resultContainer}>
-                <BodyText style={styles.resultText}>
-                    Your phone needed <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to guess the
-                    number <Text style={styles.highlight}>{props.userNumber}</Text>.
-                </BodyText>
-            </View>
-            <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
-        </View>
+        </ScrollView>
     );
 };
 
@@ -39,13 +49,13 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     imageContainer: {
-        width: 300,
-        height: 300,
-        borderRadius: 150, // half of width and height to force a circle
+        width: Dimensions.get('window').width * 0.7,
+        height: Dimensions.get('window').width * 0.7,  // Using width to force equal dimensions
+        borderRadius: Dimensions.get('window').width * 0.7 / 2, // half of width and height to force a circle
         borderWidth: 3,
         borderColor: 'black',
         overflow: 'hidden',
-        marginVertical: 30
+        marginVertical: Dimensions.get('window').height / 20,
     },
     image: {
         width: '100%',
@@ -53,11 +63,11 @@ const styles = StyleSheet.create({
     },
     resultContainer: {
         marginHorizontal: 30,
-        marginVertical: 20,
+        marginVertical: Dimensions.get('window').height / 40,
     },
     resultText: {
         textAlign: 'center',
-        fontSize: 20,
+        fontSize: Dimensions.get('window').height < 400 ? 16 : 20,
     },
     highlight: {
         color: Colors.primary,
