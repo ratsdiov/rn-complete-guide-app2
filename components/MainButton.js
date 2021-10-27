3a768503d3b0a0efc3a7aee964172897bac36cff
Keith Davidson <kbdsp@hotmail.com>
2021-10-26 21:55:07 -0400

    Start of 104, add ripple button effect on android

--- components/MainButton.js
+++ components/MainButton.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, TouchableNativeFeedback, Platform } from 'react-native';
 
 import Colors from '../constants/colors';
 
 const MainButton = props => {
+    let ButtonComponent = TouchableOpacity; // Note cap for JSX
+
+    if (Platform.OS == 'android' && Platform.Version >= 21) {
+        ButtonComponent = TouchableNativeFeedback;
+    }
+
     return (
-        <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
-            <View style={{ ...styles.button }}>
-                <Text style={styles.buttonText}>{props.children}</Text>
-            </View>
-        </TouchableOpacity>
+        <View style={styles.buttonContainer}>
+            <ButtonComponent activeOpacity={0.6} onPress={props.onPress}>
+                <View style={{ ...styles.button }}>
+                    <Text style={styles.buttonText}>{props.children}</Text>
+                </View>
+            </ButtonComponent>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
+    buttonContainer: {
+        borderRadius: 25,
+        overflow: 'hidden'  // Forces clipping of the ripple effect on android
+    },
     button: {
         backgroundColor: Colors.primary,
         paddingVertical: 12,
